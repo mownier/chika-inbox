574a82065c5bb0e5981686eb04b939568afc2488
Mounir Ybanez <rinuom91@gmail.com>
2018-02-12 22:37:15 +0800

    Implemented a raw logic on sign in/sign out.

--- ChikaInbox/ViewController.swift
+++ ChikaInbox/ViewController.swift
@@ -9,13 +9,23 @@
 import UIKit
 import ChikaCore
 import ChikaSignIn
+import ChikaFirebase
 import ChikaRegistrar
+import FirebaseCommunity
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var containerView: UIView!
     
     var inboxSceneView: UIView!
+    
+    var hasAuth: Bool {
+        guard let uid = FirebaseCommunity.Auth.auth().currentUser?.uid, !uid.isEmpty else {
+            return false
+        }
+        
+        return true
+    }
     
     override func loadView() {
         super.loadView()
@@ -24,12 +34,18 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.shadowImage = UIImage()
         navigationController?.navigationBar.isTranslucent = true
         
-        let factory = Factory()
-        let scene = factory.onSelect({ print($0) }).build()
-        inboxSceneView = scene.view
-        containerView.addSubview(inboxSceneView)
-        addChildViewController(scene)
-        scene.didMove(toParentViewController: self)
+        buildInboxScene()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if hasAuth {
+            navigationItem.leftBarButtonItem?.title = "Sign Out"
+            
+        } else {
+            navigationItem.leftBarButtonItem?.title = "Sign In"
+        }
     }
     
     override func viewDidLayoutSubviews() {
@@ -37,10 +53,36 @@ class ViewController: UIViewController {
     }
     
     @IBAction func signIn(_ sender: UIBarButtonItem) {
-        let factory = ChikaSignIn.Factory()
-        let scene = factory.withOutput(signInOutput).build()
-        scene.title = "Sign In"
-        navigationController?.pushViewController(scene, animated: true)
+        if hasAuth {
+            let presence = OfflinePresenceSwitcher()
+            let _ = presence.switchToOffline { result in
+                print("switch to offline after sign out:", result)
+            }
+            
+            let action = SignOut()
+            let _ = action.signOut { [weak self] result in
+                guard let this = self else {
+                    return
+                }
+                
+                switch result {
+                case .ok(let ok):
+                    this.navigationItem.leftBarButtonItem?.title = "Sign In"
+                    this.removeInboxScene()
+                    showAlert(withTitle: "Success", message: "\(ok)", from: this)
+                
+                case .err(let error):
+                    showAlert(withTitle: "Error", message: "\(error)", from: this)
+                }
+            }
+            
+        } else {
+            let factory = ChikaSignIn.Factory()
+            let scene = factory.withOutput(signInOutput).build()
+            scene.title = "Sign In"
+            navigationController?.pushViewController(scene, animated: true)
+        }
+
     }
     
     @IBAction func register(_ sender: UIBarButtonItem) {
@@ -61,11 +103,43 @@ class ViewController: UIViewController {
     func handleOutput(_ result: Result<OK>) {
         switch result {
         case .ok:
+            let presence = OnlinePresenceSwitcher()
+            let _ = presence.switchToOnline { result in
+                print("switch to online after sign in:", result)
+            }
+            
+            removeInboxScene()
+            buildInboxScene()
+            view.setNeedsLayout()
+            view.layoutIfNeeded()
+            navigationItem.leftBarButtonItem?.title = "Sign Out"
             navigationController?.popViewController(animated: true)
             
         case .err(let error):
             showAlert(withTitle: "Error", message: "\(error)", from: self)
         }
+    }
+    
+    func buildInboxScene() {
+        let factory = Factory()
+        let scene = factory.withUnreadChatCountTracker({ print($0) }).onSelect({ print($0) }).build()
+        inboxSceneView = scene.view
+        containerView.addSubview(inboxSceneView)
+        addChildViewController(scene)
+        scene.didMove(toParentViewController: self)
+    }
+    
+    func removeInboxScene() {
+        guard let scene = childViewControllers.filter({ $0 is Scene }).map({ $0 as! Scene }).first,
+            inboxSceneView == scene.view else {
+            return
+        }
+        
+        view.willRemoveSubview(inboxSceneView)
+        inboxSceneView.removeFromSuperview()
+        scene.removeFromParentViewController()
+        scene.didMove(toParentViewController: nil)
+        inboxSceneView = nil
     }
 
 }
