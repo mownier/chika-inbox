1f247ba0e96e3a828776aab7685c2765860c36b9
Mounir Ybanez <rinuom91@gmail.com>
2018-02-12 22:36:05 +0800

    Added offline and online switchers.

--- ChikaInbox/AppDelegate.swift
+++ ChikaInbox/AppDelegate.swift
@@ -7,16 +7,40 @@
 //
 
 import UIKit
+import ChikaCore
+import ChikaFirebase
 import FirebaseCommunity
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var onlineOperator: OnlinePresenceSwitcherOperator!
+    var offlineOperator: OfflinePresenceSwitcherOperator!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        
+        onlineOperator = OnlinePresenceSwitcherOperation()
+        offlineOperator = OfflinePresenceSwitcherOperation()
+        
         return true
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        let switcher = OnlinePresenceSwitcher()
+        let completion: (Result<OK>) -> Void = { result in
+            print(result)
+        }
+        let _ = onlineOperator.withCompletion(completion).switchToOnline(using: switcher)
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        let switcher = OfflinePresenceSwitcher()
+        let completion: (Result<OK>) -> Void = { result in
+            print(result)
+        }
+        let _ = offlineOperator.withCompletion(completion).switchToOffline(using: switcher)
     }
 
 }
